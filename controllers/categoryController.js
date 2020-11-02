4519c1a1d811f9d905d401fbe55f00491aa0db65
IanCode09 <ianlombu@protonmail.com>
2020-11-02 15:54:40 +0700

    Read Category & fetch to view Category

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,7 +1,12 @@
 const Category = require('../models/Category')
 
-const viewCategory = (req, res) => {
-    res.render('admin/category/view_category')
+const viewCategory = async(req, res) => {
+    try {
+        const category = await Category.find({})
+        res.render('admin/category/view_category', { category })
+    } catch (error) {
+        res.redirect('/admin/category')
+    }
 }
 
 const addCategory = async(req, res) => {
