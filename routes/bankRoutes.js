d9c655a8381fa9757a6ce381f915ff854bcdff6e
IanCode09 <ianlombu@protonmail.com>
2020-11-03 09:20:30 +0700

    Update bank Part 1

--- routes/bankRoutes.js
+++ routes/bankRoutes.js
@@ -4,5 +4,6 @@ const { upload } = require('../middlewares/multer')
 
 router.get('/bank', bankController.viewBank)
 router.post('/bank', upload, bankController.addBank)
+router.put('/bank', upload, bankController.editBank)
 
 module.exports = router
